357b6cb7dfcec4613aa7871c64d7ef643653aead
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-01-10 14:32:04 +0100

    add Customer model

---
+++ app/models/customer.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Datetime, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+# Customer model
+class Customer(Base):
+    __tablename__ = "customers"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    sales_contact_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
+    phone = Column(String(20), nullable=False)
+    company_name = Column(String(100), nullable=True)
+    last_contact_date = Column(Datetime, default=datetime.now())
+
+
+    # Relationships
+    sales_contact = relationship("User", back_populates="managed_customers")
+    contracts = relationship("Contrat", back_populates="customer")
