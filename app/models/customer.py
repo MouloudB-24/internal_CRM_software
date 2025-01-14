43bbdbcba7da7a8bacb6b91c523f8c452c827664
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2025-01-14 15:58:31 +0100

    update Customer model

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,25 +1,21 @@
 from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, Datetime, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-
-Base = declarative_base()
+from app import db
 
 
 # Customer model
-class Customer(Base):
+class Customer(db.Model):
     __tablename__ = "customers"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    sales_contract_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    full_name = Column(String(100), nullable=False)
-    email = Column(String(100), nullable=False)
-    phone = Column(String(20), nullable=False)
-    company_name = Column(String(100), nullable=True)
-    last_contact_date = Column(Datetime, default=datetime.now())
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    sales_contact_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    full_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    company_name = db.Column(db.String(100), nullable=True)
+    last_contact_date = db.Column(db.DateTime, default=datetime.now())
 
 
     # Relationships
-    sales_contract = relationship("User", back_populates="managed_customers")
-    contracts = relationship("Contrat", back_populates="customer")
+    sales_contact = db.relationship("User", back_populates="managed_customers")
+    contracts = db.relationship("Contract", back_populates="customer")
